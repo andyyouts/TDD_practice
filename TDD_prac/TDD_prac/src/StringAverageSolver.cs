27c30f591b37ec1567ad273ea38a5360d45429d7
andy.you~ <andy.you56@titansoft.com.sg>
2025-06-10 17:58:31 +0800

    feat(String Average): Add StringAverageSolver and corresponding tests for average calculation

---
+++ TDD_prac/TDD_prac/src/StringAverageSolver.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+
+namespace TDD_prac;
+
+public class StringAverageSolver
+{
+    private readonly Dictionary<string, int> _numberStringToInt = new()
+    {
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9},
+    };
+
+    public string GetStringAverage(string numbersAsWords)
+    {
+        if (string.IsNullOrWhiteSpace(numbersAsWords))
+        {
+            return "n/a";
+        }
+
+        var wordNumbers = numbersAsWords.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        return !TryParseAllNumbers(wordNumbers, out var sum)
+            ? "n/a"
+            : CalculateAverage(sum, wordNumbers.Length);
+    }
+
+    private bool TryParseAllNumbers(string[] wordNumbers, out int sum)
+    {
+        sum = 0;
+
+        foreach (var wordNumber in wordNumbers)
+        {
+            if (!_numberStringToInt.TryGetValue(wordNumber, out var value))
+            {
+                return false;
+            }
+
+            sum += value;
+        }
+
+        return true;
+    }
+
+    private static string CalculateAverage(int sum, int count)
+    {
+        var average = Math.Floor(sum / (double)count);
+        return average.ToString(CultureInfo.InvariantCulture);
+    }
+    
+}
---
+++ TDD_prac/TDD_prac/tests/StringAverageTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+
+namespace TDD_prac.tests;
+
+public class StringAverageTests
+{
+    private StringAverageSolver _stringAverageSolver;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _stringAverageSolver = new StringAverageSolver();
+    }
+
+    [Test]
+    public void should_return_average_number_for_all_valid_number_strings()
+    {
+        GivenNumbersOfStrings("one two three").Should().Be("2");
+    }
+
+    [Test]
+    public void should_return_na_for_larger_than_nine_number_string()
+    {
+        GivenNumbersOfStrings("one two three ten").Should().Be("n/a");
+    }
+
+    [Test]
+    public void should_return_na_for_empty_strings()
+    {
+        GivenNumbersOfStrings("").Should().Be("n/a");
+    }
+
+    private string GivenNumbersOfStrings(string numbersAsStrings)
+    {
+        return _stringAverageSolver.GetStringAverage(numbersAsStrings);
+    }
+}
