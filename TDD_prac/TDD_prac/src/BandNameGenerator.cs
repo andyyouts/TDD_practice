1ecf042973641d93b4f3bf61422dbba9585d5908
andy.you~ <andy.you56@titansoft.com.sg>
2025-06-13 18:35:39 +0800

    feat(Band Name Generator): Implement BandNameGenerator with validation and tests

---
+++ TDD_prac/TDD_prac/src/BandNameGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace TDD_prac;
+
+public class BandNameGenerator
+{
+    public string GenerateBandName(string band)
+    {
+        if (band.Split(" ").Length > 1)
+        {
+            throw new Exception("Band name should not contain multiple words.");
+        }
+
+        var bandNameWithoutFirstCharacter = band[1..].ToLower();
+        var capitalizedBandName = (char.ToUpper(band[0]) + bandNameWithoutFirstCharacter);
+
+        if (band.First() != band.Last())
+        {
+            return "The " + capitalizedBandName;
+        }
+
+        return capitalizedBandName + bandNameWithoutFirstCharacter;
+    }
+}
---
+++ TDD_prac/TDD_prac/tests/BandNameGeneratorTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+namespace TDD_prac.tests;
+
+public class BandNameGeneratorTests
+{
+    private BandNameGenerator _bandNameGenerator;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _bandNameGenerator = new BandNameGenerator();
+    }
+
+    [Test]
+    public void should_throw_exception_if_band_name_has_multiple_words()
+    {
+        var exception = Assert.Throws<Exception>(() => _bandNameGenerator.GenerateBandName("the beatles"));
+        exception.Message.Should().Be("Band name should not contain multiple words.");
+    }
+    
+    [Test]
+    public void should_return_the_capitalized_name_for_band_has_different_first_and_last_character()
+    {
+        _bandNameGenerator.GenerateBandName("beatles").Should().Be("The Beatles");
+    }
+
+    [Test]
+    public void should_return_the_repeated_band_for_band_has_same_first_and_last_character()
+    {
+        _bandNameGenerator.GenerateBandName("abba").Should().Be("Abbabba");
+    }
+    
+}
