3544098dec042bbc5360d99a227934a099f13188
andy.you~ <andy.you56@titansoft.com.sg>
2025-06-13 18:14:58 +0800

    feat(Dead Ants Solver): Implement DeadAntsSolver and add tests for counting scattered bits

---
+++ TDD_prac/TDD_prac/src/DeadAntsSolver.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace TDD_prac;
+
+public class DeadAntsSolver
+{
+    private readonly Dictionary<char, int> _letterCounts = new()
+    {
+        { 'a', 0 },
+        { 'n', 0 },
+        { 't', 0 }
+    };
+
+    public int CountDeadAnts(string antsString)
+    {
+        if (string.IsNullOrWhiteSpace(antsString))
+        {
+            return 0;
+        }
+
+        var antBits = antsString.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+        CountScatteredBits(antBits);
+
+        return _letterCounts.Values.Max();
+    }
+
+    private void CountScatteredBits(string[] antBits)
+    {
+        foreach (var antBit in antBits)
+        {
+            if (antBit == "ant")
+            {
+                continue;
+            }
+            
+            var scatteredBits = antBit.Contains("ant")
+                ? antBit.Replace("ant", "")
+                : antBit;
+
+            foreach (var character in scatteredBits)
+            {
+                _letterCounts[character]++;
+            }
+        }
+    }
+}
---
+++ TDD_prac/TDD_prac/tests/DeadAntsTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+namespace TDD_prac.tests;
+
+public class DeadAntsTests
+{
+    private DeadAntsSolver _deadAntsSolver;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _deadAntsSolver = new DeadAntsSolver();
+    }
+
+    [Test]
+    public void should_return_zero_for_empty_string()
+    {
+        _deadAntsSolver.CountDeadAnts(" ").Should().Be(0);
+    }
+
+    [Test]
+    public void should_return_zero_for_no_scattered_bits()
+    {
+        _deadAntsSolver.CountDeadAnts("ant...ant").Should().Be(0);
+    }
+
+    [TestCase("...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t", 3)]
+    [TestCase("....ant...a.ant..anatttt.", 4)]
+    public void should_return_numbers_of_scattered_bits_(string antsStrings, int expectedCount)
+    {
+        _deadAntsSolver.CountDeadAnts(antsStrings).Should().Be(expectedCount);
+    }
+    
+}
