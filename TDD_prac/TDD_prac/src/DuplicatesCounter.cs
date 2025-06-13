25a6784bc51df361011d14993c94d4c4970a8723
andy.you~ <andy.you56@titansoft.com.sg>
2025-06-13 20:29:39 +0800

    feat(Duplicates Counter): Implement DuplicatesCounter class with duplicate counting logic and add corresponding tests

---
+++ TDD_prac/TDD_prac/src/DuplicatesCounter.cs
@@ -0,0 +1,26 @@
+﻿namespace TDD_prac;
+
+public class DuplicatesCounter
+{
+    private readonly Dictionary<char, int> _charCounter = new();
+
+    public int CountDuplicates(string input)
+    {
+        var duplicateCounts = 0;
+
+        foreach (var lowerCharacter in input.Select(char.ToLower))
+        {
+            if (!_charCounter.TryGetValue(lowerCharacter, out var value))
+            {
+                _charCounter.Add(lowerCharacter, 1);
+            }
+            
+            else if (value == 1)
+            {
+                _charCounter[lowerCharacter]++;
+                duplicateCounts++;
+            }
+        }
+        return duplicateCounts;
+    }
+}
