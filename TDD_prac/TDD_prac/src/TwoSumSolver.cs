045aed8b5131b751982e7528bc98a0385b64783f
andy.you~ <andy.you56@titansoft.com.sg>
2025-06-16 00:29:10 +0800

    feat(Two Sum Solver): Implement TwoSumSolver class with logic to find indexes for two sum and add corresponding tests

---
+++ TDD_prac/TDD_prac/src/TwoSumSolver.cs
@@ -0,0 +1,19 @@
+﻿namespace TDD_prac;
+
+public class TwoSumSolver
+{
+    public int[] GetIndexesForTwoSum(int[] ints, int i)
+    {
+        var indexes = new Dictionary<int, int>();
+        for (var j = 0; j < ints.Length; j++)
+        {
+            var complement = i - ints[j];
+            if (indexes.TryGetValue(complement, out var value))
+            {
+                return [value, j];
+            }
+            indexes[ints[j]] = j;
+        }
+        throw new ArgumentException("No two sum solution");
+    }
+}
---
+++ TDD_prac/TDD_prac/tests/TwoSumSolverTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+
+namespace TDD_prac.tests;
+
+public class TwoSumSolverTests
+{
+    private TwoSumSolver _twoSumSolver;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _twoSumSolver = new TwoSumSolver();
+    }
+
+    [TestCase(new[] { 2, 7, 3, 10 }, 9, new[] { 0, 1 })]
+    [TestCase(new[] { 100, 3, 20, 77 }, 23, new[] { 1, 2 })]
+    [TestCase(new[] { 1, 2, 3, 7, 10, 12, 44, 29, 78, 1000 }, 88, new[] { 4, 8 })]
+    public void should_return_indexes_for_two_sum(int[] ints, int sum, IEnumerable<int> expectation)
+    {
+        _twoSumSolver.GetIndexesForTwoSum(ints, sum)
+            .Should()
+            .BeEquivalentTo(expectation);
+    }
+
+    [Test]
+    public void should_throw_exception_if_no_two_sum_can_be_formed()
+    {
+        var exception = Assert.Throws<ArgumentException>(() => _twoSumSolver.GetIndexesForTwoSum([1, 2, 3, 4, 5], 10));
+        exception.Message.Should().Be("No two sum solution");
+    }
+}
