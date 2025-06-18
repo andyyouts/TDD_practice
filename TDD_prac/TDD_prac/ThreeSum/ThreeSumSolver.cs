8a7caa58959e677c257be6baff9ac7734efe3dcd
andy.you~ <andy.you56@titansoft.com.sg>
2025-06-18 22:47:09 +0800

    feat(Three Sum Solver): Add ThreeSumSolver implementation and tests

---
+++ TDD_prac/TDD_prac/ThreeSum/ThreeSumSolver.cs
@@ -0,0 +1,121 @@
+﻿namespace TDD_prac.ThreeSum;
+
+public class ThreeSumSolver
+{
+    public IList<IList<int>> SolveThreeSum(int[]? ints, bool ascending = true)
+    {
+        ArgumentNullException.ThrowIfNull(ints);
+
+        if (ints.Length < 3)
+        {
+            return new List<IList<int>>();
+        }
+        
+        return ascending ? SolveByDescending(ints) : SolveByAscending(ints);    
+    }
+
+    private IList<IList<int>> SolveByAscending(int[] ints)
+    {
+        var result = new List<IList<int>>();
+
+        Array.Sort(ints);
+        
+        for (var i = 0; i < ints.Length - 2; i++)
+        {
+            if (i > 0 && ints[i] == ints[i - 1])
+            {
+                continue;
+            }
+
+            var left = i + 1;
+            var right = ints.Length - 1;
+
+            while (left < right)
+            {
+                var sum = ints[i] + ints[left] + ints[right];
+
+                switch (sum)
+                {
+                    case 0:
+                    {
+                        result.Add(new List<int> { ints[i], ints[left], ints[right] });
+                        while (left < right && ints[left] == ints[left + 1])
+                        {
+                            left++; 
+                        }
+
+                        while (left < right && ints[right] == ints[right - 1])
+                        {
+                            right--; 
+                        }
+
+                        left++;
+                        right--;
+                        break;
+                    }
+                    case < 0:
+                        left++;
+                        break;
+                    default:
+                        right--;
+                        break;
+                }
+            }
+        }
+
+        return result;
+    }
+    
+    private IList<IList<int>> SolveByDescending(int[] ints)
+    {
+        var result = new List<IList<int>>();
+
+        ints = ints.OrderDescending().ToArray();
+        
+        for (var i = 0; i < ints.Length - 2; i++)
+        {
+            if (i > 0 && ints[i] == ints[i - 1])
+            {
+                continue;
+            }
+
+            var left = i + 1;
+            var right = ints.Length - 1;
+
+            while (left < right)
+            {
+                var sum = ints[i] + ints[left] + ints[right];
+
+                switch (sum)
+                {
+                    case 0:
+                    {
+                        result.Add(new List<int> { ints[i], ints[left], ints[right] });
+                        while (left < right && ints[left] == ints[left + 1])
+                        {
+                            left++; 
+                        }
+
+                        while (left < right && ints[right] == ints[right - 1])
+                        {
+                            right--; 
+                        }
+
+                        left++;
+                        right--;
+                        break;
+                    }
+                    case > 0:
+                        left++;
+                        break;
+                    default:
+                        right--;
+                        break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
---
+++ TDD_prac/TDD_prac/ThreeSum/ThreeSumSolverTests.cs
@@ -0,0 +1,102 @@
+﻿using FluentAssertions;
+
+namespace TDD_prac.ThreeSum;
+
+public class ThreeSumSolverTests
+{
+    private ThreeSumSolver _threeSumSolver;
+
+    private readonly List<List<IList<int>>> _possibleSolutions =
+    [
+        new()
+        {
+            new List<int> { -1, -1, 2 },
+            new List<int> { -1, 0, 1 }
+        },
+
+        new()
+        {
+            new List<int> { -1, 0, 1 },
+            new List<int> { -1, -1, 2 }
+        },
+
+        new()
+        {
+            new List<int> { 2, -1, -1 },
+            new List<int> { 1, 0, -1 }
+        },
+
+        new()
+        {
+            new List<int> { 1, 0, -1 },
+            new List<int> { 2, -1, -1 }
+        },
+
+        new()
+        {
+            new List<int> { 2, -1, -1 },
+            new List<int> { -1, 0, 1 }
+        },
+
+        new()
+        {
+            new List<int> { -1, 0, 1 },
+            new List<int> { 2, -1, -1 }
+        },
+
+        new()
+        {
+            new List<int> { -1, -1, 2 },
+            new List<int> { 1, 0, -1 }
+        },
+
+        new()
+        {
+            new List<int> { 1, 0, -1 },
+            new List<int> { -1, -1, 2 }
+        }
+    ];
+
+    [SetUp]
+    public void SetUp()
+    {
+        _threeSumSolver = new ThreeSumSolver();
+    }
+
+    [Test]
+    public void should_throw_exception_when_input_list_is_null()
+    {
+        Assert.Throws<ArgumentNullException>(() => _threeSumSolver.SolveThreeSum(null));
+    }
+
+    [Test]
+    public void should_return_empty_list_when_no_three_sum_solution()
+    {
+        var threeSum = _threeSumSolver.SolveThreeSum([1, 2, 3]);
+        threeSum.Should().BeEquivalentTo(new List<IList<int>>());
+    }
+
+    [Test]
+    public void should_return_solution_when_there_is_only_one_solution()
+    {
+        var threeSum = _threeSumSolver.SolveThreeSum([0, 0, 0]);
+        threeSum.Should().BeEquivalentTo(new List<IList<int>> { new List<int> { 0, 0, 0 } });
+    }
+
+    [Test]
+    public void should_return_solution_regardless_of_order_if_there_are_many()
+    {
+        var threeSumByAscending = _threeSumSolver.SolveThreeSum([-1, 0, 1, 2, -1, -4]);
+        var threeSumByDescending = _threeSumSolver.SolveThreeSum([-1, 0, 1, 2, -1, -4], false);
+
+        IsValidSolution(threeSumByAscending, threeSumByDescending);
+    }
+
+    private void IsValidSolution(params IList<IList<int>>[] results)
+    {
+        foreach (var result in results)
+        {
+            _possibleSolutions.Should().ContainEquivalentOf(result);
+        }
+    }
+}
