fc43a50f0f0050bd184c016216a2f02b3e9c6282
andy.you~ <andy.you56@titansoft.com.sg>
2025-06-21 01:16:05 +0800

    feat(Next Bigger Number Solver): Implement NextBiggerNumberSolver and corresponding tests

---
+++ TDD_prac/TDD_prac/NextBiggerNumber/NextBiggerNumberSolver.cs
@@ -0,0 +1,83 @@
+﻿namespace TDD_prac.NextBiggerNumber;
+
+public class NextBiggerNumberSolver
+{
+    private const int CannotGenerateNextBiggerNumber = -1;
+
+    public int GetNextBiggerNumber(int integer)
+    {
+        if (TryConvertToValidString(integer, out var integerAsString))
+        {
+            return CannotGenerateNextBiggerNumber;
+        }
+
+        var firstDecreasingIndex = FindFirstDecreasingIndex(integerAsString);
+
+        if (IsStringInDescendingOrder(firstDecreasingIndex))
+        {
+            return CannotGenerateNextBiggerNumber;
+        }
+
+        var nextLargerIndex = FindNextLargerIndex(integerAsString, firstDecreasingIndex);
+
+        return GenerateNextBiggerNumber(integerAsString, firstDecreasingIndex, nextLargerIndex);
+    }
+
+    private static bool IsStringInDescendingOrder(int firstDecreasingIndex)
+    {
+        return firstDecreasingIndex == 0;
+    }
+
+    private static bool TryConvertToValidString(int integer, out string s)
+    {
+        s = integer.ToString();
+        
+        return s.Length == 1;
+    }
+
+    private static int GenerateNextBiggerNumber(string s, int firstDecreasingIndex, int nextLargerIndex)
+    {
+        var charArray = s.ToCharArray();
+        (charArray[firstDecreasingIndex - 1], charArray[nextLargerIndex]) = (charArray[nextLargerIndex], charArray[firstDecreasingIndex - 1]);
+        var nextPart = charArray[firstDecreasingIndex..].ToList();
+        nextPart.Sort();
+        var result = new string(charArray.Take(firstDecreasingIndex).ToArray()) + new string(nextPart.ToArray());
+
+        if (int.TryParse(result, out var nextBiggerNumber))
+        {
+            return nextBiggerNumber;
+        }
+
+        return CannotGenerateNextBiggerNumber;
+    }
+
+    private static int FindNextLargerIndex(string s, int i)
+    {
+        var j = 0;
+
+        for (j = s.Length - 1; j >= i - 1; j--)
+        {
+            if (s[i - 1] < s[j])
+            {
+                break;
+            }
+        }
+
+        return j;
+    }
+
+    private static int FindFirstDecreasingIndex(string s)
+    {
+        var i = 0;
+
+        for (i = s.Length - 1; i > 0; i--)
+        {
+            if (s[i] > s[i - 1])
+            {
+                break;
+            }
+        }
+
+        return i;
+    }
+}
---
+++ TDD_prac/TDD_prac/NextBiggerNumber/NextBiggerNumberSolverTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+
+namespace TDD_prac.NextBiggerNumber;
+
+public class NextBiggerNumberSolverTests
+{
+    private NextBiggerNumberSolver _nextBiggerNumberSolver;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _nextBiggerNumberSolver = new NextBiggerNumberSolver();
+    }
+
+    [Test]
+    public void should_return_negative_one_for_string_with_single_digit()
+    {
+        var nextBiggerNumber = _nextBiggerNumberSolver.GetNextBiggerNumber(4);
+        nextBiggerNumber.Should().Be(-1);
+    }
+
+    [TestCase(444, TestName = "three digit number with same digits")]
+    [TestCase(44444444, TestName = "eight digit number with same digits")]
+    public void should_return_negative_one_for_string_with_all_same_digits(int sameDigitInt)
+    {
+        var nextBiggerNumber = _nextBiggerNumberSolver.GetNextBiggerNumber(sameDigitInt);
+        nextBiggerNumber.Should().Be(-1);
+    }
+
+    [TestCase(987654321, TestName = "consecutive sequence in descending order")]
+    [TestCase(97531, TestName = "descending order with common difference two")]
+    [TestCase(986532, TestName = "descending order with random differences")]
+    public void should_return_negative_one_for_string_in_descending_order(int descendingInt)
+    {
+        var nextBiggerNumber = _nextBiggerNumberSolver.GetNextBiggerNumber(descendingInt);
+
+        nextBiggerNumber.Should().Be(-1);
+    }
+
+    [TestCase(123456789, 123456798, TestName = "consecutive sequence in ascending order")]
+    [TestCase(13579, 13597, TestName = "ascending order with common difference two")]
+    [TestCase(235689, 235698, TestName = "ascending order with random differences")]
+    public void should_return_last_two_digits_swap_for_string_in_ascending_order(int ascendingInt, int expectedInt)
+    {
+        var nextBiggerNumber = _nextBiggerNumberSolver.GetNextBiggerNumber(ascendingInt);
+
+        nextBiggerNumber.Should().Be(expectedInt);
+    }
+
+    [TestCase(513, 531, TestName = "unsorted three digit number integer")]
+    [TestCase(2017, 2071, TestName = "unsorted four digit number integer")]
+    [TestCase(218765, 251678, TestName = "unsorted six digit number integer")]
+    public void should_return_next_bigger_number_for_unsorted_string(int integer, int expectedInt)
+    {
+        var nextBiggerNumber = _nextBiggerNumberSolver.GetNextBiggerNumber(integer);
+
+        nextBiggerNumber.Should().Be(expectedInt);
+    }
+
+    [Test]
+    public void should_return_negative_one_if_result_overflows()
+    {
+        var nextBiggerNumber = _nextBiggerNumberSolver.GetNextBiggerNumber(2147483641);
+        
+        nextBiggerNumber.Should().Be(-1);
+    }
+}
