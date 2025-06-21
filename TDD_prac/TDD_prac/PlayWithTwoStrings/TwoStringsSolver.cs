b5bf37a7cfe23da9cee007a50428bfcf1affa028
andy.you~ <andy.you56@titansoft.com.sg>
2025-06-21 22:41:36 +0800

    feat(TwoStringsSolver): Implement logic to combine two strings with case adjustments and add corresponding tests

---
+++ TDD_prac/TDD_prac/PlayWithTwoStrings/TwoStringsSolver.cs
@@ -0,0 +1,42 @@
+﻿namespace TDD_prac.PlayWithTwoStrings;
+using System.Linq;
+
+public static class TwoStringsSolver
+{
+    private static Dictionary<char, int> Counter = new();
+    public static string Solve(string str1, string str2)
+    {
+        var changedA = SwapCases(str1, str2);
+        var changedB = SwapCases(str2, str1);
+        return string.Concat(changedA, changedB);;
+    }
+
+    private static string SwapCases(string source, string target)
+    {
+        CountCharacterOccurrences(target);
+        var adjustedCharArray = source.Select(GetAdjustedCases).ToArray();
+        return new string(adjustedCharArray);
+    }
+
+    private static void CountCharacterOccurrences(string target)
+    {
+        Counter = target
+            .GroupBy(char.ToLower)
+            .ToDictionary(g => g.Key, g => g.Count());
+    }
+
+    private static char GetAdjustedCases(char c)
+    {
+        var lower = char.ToLower(c);
+
+        if (Counter.TryGetValue(lower, out var value) && value % 2 == 1)
+        {
+            // Odd number of matches => swap case
+            return char.IsUpper(c)
+                ? char.ToLower(c)
+                : char.ToUpper(c);
+        }
+
+        return c;
+    }
+}
---
+++ TDD_prac/TDD_prac/PlayWithTwoStrings/TwoStringsSolverTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+
+namespace TDD_prac.PlayWithTwoStrings;
+
+public class TwoStringsSolverTests
+{
+    [Test]
+    public void should_return_empty_strings_if_two_strings_all_empty()
+    {
+        var combinedString = CombineTwoStrings("", "");
+        CombinedStringShouldBe(combinedString, "");
+    }
+
+    [TestCase("a", "", "a", TestName = "the second string is empty")]
+    [TestCase("", "b", "b", TestName = "the first string is empty")]
+    public void should_return_the_other_string_if_one_string_is_empty(string str1, string str2, string expectedCombinedString)
+    {
+        var combinedString = CombineTwoStrings(str1, str2);
+        CombinedStringShouldBe(combinedString, expectedCombinedString);
+    }
+
+    [TestCase("abc", "cef", "abCCef", TestName = "strings with distinct letters")]
+    [TestCase("hello", "world", "heLLOwOrld", TestName = "strings with overlapping letters")]
+    [TestCase("test", "test", "tESttESt", TestName = "identical strings")]
+    [TestCase("a", "a", "AA", TestName = "single character")]
+    public void should_handle_basic_string_combinations(string str1, string str2, string expectedCombinedString)
+    {
+        var combinedString = CombineTwoStrings(str1, str2);
+        CombinedStringShouldBe(combinedString, expectedCombinedString);
+    }
+
+    [TestCase("abc123", "123def", "abc123123def", TestName = "letters and numbers")]
+    [TestCase("!@#", "$%^", "!@#$%^", TestName = "only special characters")]
+    [TestCase("Hello123!", "World456?", "HeLLO123!WOrld456?", TestName = "mixed content")]
+    public void should_handle_non_letter_characters(string str1, string str2, string expectedCombinedString)
+    {
+        var combinedString = CombineTwoStrings(str1, str2);
+        CombinedStringShouldBe(combinedString, expectedCombinedString);
+    }
+
+    [TestCase("AABB", "AABB", "AABBAABB", TestName = "even occurrences")]
+    [TestCase("aaa", "a", "AAAA", TestName = "odd count repeated characters")]
+    [TestCase("aaa", "aa", "aaaAA", TestName = "even count repeated characters")]
+    public void should_handle_repeated_characters(string str1, string str2, string expectedCombinedString)
+    {
+        var combinedString = CombineTwoStrings(str1, str2);
+        CombinedStringShouldBe(combinedString, expectedCombinedString);
+    }
+
+    [TestCase("café", "cafe", "CAFéCAFe", TestName = "unicode characters")]
+    [TestCase("hello world", "world hello", "HELLo WoRLDWoRLD HELLo", TestName = "spaces and punctuation")]
+    [TestCase("Programming", "grammar", "ProGramminGgrAmmAr", TestName = "complex case swapping")]
+    public void should_handle_special_cases(string str1, string str2, string expectedCombinedString)
+    {
+        var combinedString = CombineTwoStrings(str1, str2);
+        CombinedStringShouldBe(combinedString, expectedCombinedString);
+    }
+
+    private static void CombinedStringShouldBe(string solve, string expectedCombinedString)
+    {
+        solve.Should().Be(expectedCombinedString);
+    }
+
+    private static string CombineTwoStrings(string str1, string str2)
+    {
+        return TwoStringsSolver.Solve(str1, str2);
+    }
+}
